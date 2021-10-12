6cc99a271278b40a1cd973878dd564baf6689d8d
madcow95 <lobasketve@naver.com>
2021-10-12 22:46:20 +0900

    로그인 페이지만 그냥

--- src/main/java/com/example/demo/controller/MainController.java
+++ src/main/java/com/example/demo/controller/MainController.java
@@ -6,17 +6,16 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("/sample")
-public class SampleController {
-    @GetMapping("/first")
-    public String sampleTest(Model model) {
-        model.addAttribute("name", "최광우");
-        return "sample";
+@RequestMapping("/main")
+public class MainController {
+    @GetMapping("/home")
+    public String sampleTest() {
+        return "home";
     }
 
-    @GetMapping("/second")
+    @GetMapping("/login")
     public String sampleTest2(Model model) {
         model.addAttribute("name", "이걸 또하고 있을줄이야");
-        return "secondSample";
+        return "login";
     }
 }
